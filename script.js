6b415e1b194b389efd2df41cd595aad04b95d723
ilathem <isaiahlathem@gmail.com>
2024-03-05 15:14:52 -0600

    Finish tic-tac-toe

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const GameBoard = (function() {
+    console.log('setting up board')
     let emptySpots = 9;
     const board = new Array(3);
     for (let i = 0; i < board.length; i++) {
         board[i] = new Array(3).fill('?');
     }
-
 
     const printBoard = () => {
         let str = '';
@@ -17,24 +17,29 @@ const GameBoard = (function() {
         console.log(str);
     } 
 
+    printBoard();
+
     const placePiece = (piece, row, col) => {
+        console.log('placing piece')
+        console.log({piece, row, col})
         board[row][col] = piece;
         emptySpots--;
     }
 
     const checkBoard = () => {
         const checkIfMatching = (var1, var2, var3) => {
+            if (var1 === '?' &&
+                var2 === '?' &&
+                var3 === '?'
+            ) {
+                return false;
+            }
             if ((var1 === var2) && (var2 === var3)) {
                 return true;
             } else return false;
         }
         const checkRows = () => {
             for (let row = 0; row < board.length; row++) {
-                console.log(
-                    board[row][0],
-                    board[row][1],
-                    board[row][2],
-                )
                 if (checkIfMatching(
                     board[row][0],
                     board[row][1],
@@ -89,16 +94,34 @@ const GameBoard = (function() {
             draw
         })
         if (draw) return draw;
-        if (rows !== '?') return rows;
-        if (cols !== '?') return cols 
-        if (diags !== '?') return diags; 
+        if (rows !== '') return rows;
+        if (cols !== '') return cols 
+        if (diags !== '') return diags; 
         return ""
+    }
+
+    const placeOpen = (row, col) => {
+        if (board[row][col] === '?') return true;
+        return false;
+    }
+
+    const resetBoard = () => {
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board.length; j++) {
+                board[i][j] = '?';
+            }
+        }
+        emptySpots = 9;
+        console.log("Board Reset...")
+        printBoard();
     }
 
     return {
         printBoard,
         placePiece,
         checkBoard,
+        placeOpen,
+        resetBoard,
     }
 })();
 
@@ -126,6 +149,9 @@ const PlayerX = createPlayer('X');
 const Game = (function() {
     const players = [PlayerX, PlayerO];
     const board = GameBoard;
+    let currentPlayer = 0;
+    let gameOver = false;
+    console.log('setting up game')
 
     const printPlayers = () => {
         let str = '';
@@ -161,24 +187,110 @@ const Game = (function() {
         }
     }
 
+    const getCurrentPlayer = () => {
+        return players[currentPlayer].getPlayerName();
+    }
+
+    const getCurrentPiece = () => {
+        return players[currentPlayer].getPlayerPiece();
+    }
+
+    const placePiece = (row, col) => {
+        board.placePiece(players[currentPlayer].getPlayerPiece(), row, col);
+        board.printBoard();
+        console.log(board.checkBoard());
+        currentPlayer = (currentPlayer + 1) % players.length;
+        console.log(currentPlayer)
+    }
+
+    const reset = () => {
+        currentPlayer = 0;
+        gameOver = false;
+        console.log('reset game')
+        console.log(gameOver);
+    }
+
+    const checkEndGame = () => {
+        const endGameState = GameBoard.checkBoard();
+        console.log(endGameState)
+        if (endGameState) gameOver = true;
+        return endGameState;
+    }
+
+    const getGameOver = () => {
+        return gameOver;
+    }
+
     return {
         printPlayers,
         start,
+        placePiece,
+        getCurrentPlayer,
+        getCurrentPiece,
+        getGameOver,
+        checkEndGame,
+        reset,
     }
 })()
 
 const GameDisplay = (function() {
+    // Define globals
+    const board = new Array(3);
+    const message = document.createElement('p');
+    const newGameBtn = document.createElement('button');
+    newGameBtn.classList.add('newGameBtn');
+    newGameBtn.innerText = 'New Game'
+    message.classList.add('message');
+    message.innerText = (`${Game.getCurrentPlayer()}, your turn!`);
+    // Define event listeners
+    const gridItemClick = (event) => {
+        const i = event.target.getAttribute('i')
+        const j = event.target.getAttribute('j')
+        console.log(`Game.gameOver: ${Game.getGameOver()}`)
+        if (GameBoard.placeOpen(i, j) && !Game.getGameOver()) {
+            event.target.innerText = Game.getCurrentPiece();
+            Game.placePiece(i, j)
+            const endGameState = Game.checkEndGame();
+            if (endGameState) {
+                endGameState === 'draw' ? 
+                message.innerText = 'Draw!'
+                :
+                message.innerText = `Player ${endGameState} won!`
+            } else {
+                message.innerText = (`${Game.getCurrentPlayer()}, your turn!`);
+            }
+        }
+    }
+    const resetUIBoard = () => {
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board.length; j++) {
+                board[i][j].innerText = '';
+            }
+        }
+        message.innerText = (`${Game.getCurrentPlayer()}, your turn!`);
+    }
+    const newGame = () => {
+        GameBoard.resetBoard();
+        Game.reset();
+        resetUIBoard();
+    }
+    newGameBtn.addEventListener('click', () => newGame());
+    // Set up UI
     const main = document.querySelector("main");
+    main.appendChild(message);
+    main.appendChild(newGameBtn);
     const boardDiv = document.createElement('div');
     boardDiv.classList.add('grid');
-    const board = new Array(3);
     for (let i = 0; i < board.length; i++) {
         board[i] = new Array(3);
         for (let j = 0; j < board[i].length; j++) {
             board[i][j] = document.createElement("div");
             board[i][j].classList.add('grid-item');
             board[i][j].classList.add(`item${i}${j}`);
-            board[i][j].innerText = ' ';
+            board[i][j].innerText = '';
+            board[i][j].addEventListener('click', event => gridItemClick(event));
+            board[i][j].setAttribute('i', i)
+            board[i][j].setAttribute('j', j)
             boardDiv.appendChild(board[i][j]);
         }
     }
